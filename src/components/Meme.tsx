dde5176fa331fe9c9699e5647cb39897384994b9
Taha <tsid2181@gmail.com>
2023-07-16 14:44:38 -0400

    fixed error

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 
 export default function Meme() {
     const [meme, setMeme] = useState({
@@ -11,10 +11,10 @@ export default function Meme() {
 
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
-        .then(data => setAllMemes(data.data.memes))
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
     }, [])
-    
+
     function getMemeImage() {
         const randomNumber = Math.floor(Math.random() * allMemes.length)
         const url = allMemes[randomNumber].url
@@ -25,9 +25,9 @@ export default function Meme() {
     }
 
     function handleChange(event: any) {
-        const {name, value} = event.target
+        const { name, value } = event.target
         setMeme(prevMeme => ({
-            ...meme,
+            ...prevMeme,
             [name]: value,
         }))
     }
@@ -35,29 +35,29 @@ export default function Meme() {
     return (
         <main className="p-9">
             <div className="grid grid-rows-2 gap-4 mb-4">
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     placeholder="Top text" className="h-9 font-karla border border-solid border-slate-300 rounded-md indent-1"
                     name="topText"
                     onChange={handleChange}
                     value={meme.topText}
                 />
-                
-                <input 
-                    type="text" 
+
+                <input
+                    type="text"
                     placeholder="Bottom text" className="h-9 font-karla border border-solid border-slate-300 rounded-md indent-1"
                     name="bottomText"
                     onChange={handleChange}
-                    value={meme.bottomText} 
+                    value={meme.bottomText}
                 />
-                
-                <button 
-                className="h-10 col-span-2 font-karla bg-gradient-to-r from-purple-800 to-fuchsia-600 border-none text-white cursor-pointer" 
-                onClick={getMemeImage}>
+
+                <button
+                    className="h-10 col-span-2 font-karla bg-gradient-to-r from-purple-800 to-fuchsia-600 border-none text-white cursor-pointer"
+                    onClick={getMemeImage}>
                     Get a new meme image 🖼
                 </button>
             </div>
-            
+
             <div className="relative">
                 <img src={meme.randomImage} className="max-w-full rouned-sm" />
                 <h2 className="meme-text top-0">{meme.topText}</h2>
